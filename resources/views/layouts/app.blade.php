63cb723e3d1000042f279b71fd6bbd9bed9940c5
ogiki477 <omojick88moses@gmail.com>
2024-10-24 18:24:25 +0300

    Added the the project Setup

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,24 +10,24 @@
   <meta content="" name="keywords">   
 
   <!-- Favicons -->
-  <link href="{{url('public/assets/img/sacco2.jpeg' )}}" rel="icon">
-  <link href="{{url('public/assets/img/chaticon.png' )}}" rel="apple-touch-icon">
+  <link href="{{url('assets/img/sacco2.jpeg' )}}" rel="icon">
+  <link href="{{url('assets/img/chaticon.png' )}}" rel="apple-touch-icon">
 
   <!-- Google Fonts -->
   <link href="https://fonts.gstatic.com" rel="preconnect">
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
   <!-- Vendor CSS Files -->
-  <link href="{{url('public/assets/vendor/bootstrap/css/bootstrap.min.css' )}}" rel="stylesheet">
-  <link href="{{url('public/assets/vendor/bootstrap-icons/bootstrap-icons.css' )}}" rel="stylesheet">
-  <link href="{{url('public/assets/vendor/boxicons/css/boxicons.min.css' )}}" rel="stylesheet">
-  <link href="{{url('public/assets/vendor/quill/quill.snow.css' )}}" rel="stylesheet">
-  <link href="{{url('public/assets/vendor/quill/quill.bubble.css' )}}" rel="stylesheet">
-  <link href="{{url('public/assets/vendor/remixicon/remixicon.css' )}}" rel="stylesheet">
-  <link href="{{url('public/assets/vendor/simple-datatables/style.css' )}}" rel="stylesheet">
+  <link href="{{url('assets/vendor/bootstrap/css/bootstrap.min.css' )}}" rel="stylesheet">
+  <link href="{{url('assets/vendor/bootstrap-icons/bootstrap-icons.css' )}}" rel="stylesheet">
+  <link href="{{url('assets/vendor/boxicons/css/boxicons.min.css' )}}" rel="stylesheet">
+  <link href="{{url('assets/vendor/quill/quill.snow.css' )}}" rel="stylesheet">
+  <link href="{{url('assets/vendor/quill/quill.bubble.css' )}}" rel="stylesheet">
+  <link href="{{url('assets/vendor/remixicon/remixicon.css' )}}" rel="stylesheet">
+  <link href="{{url('assets/vendor/simple-datatables/style.css' )}}" rel="stylesheet">
 
   <!-- Template Main CSS File -->
-  <link href="{{url('public/assets/css/style.css' )}}" rel="stylesheet">
+  <link href="{{url('assets/css/style.css' )}}" rel="stylesheet">
 
   <!-- =======================================================
   * Template Name: NiceAdmin
@@ -55,7 +55,7 @@
 
               <div class="d-flex justify-content-center py-4">
                 <a href="" class="logo d-flex align-items-center w-auto">
-                  <img src="{{url('public/assets/img/sacco2.jpeg' )}}" alt="">
+                  <img src="{{url('assets/img/sacco2.jpeg' )}}" alt="">
                   <span class="d-none d-lg-block">SACCO</span>
                 </a>
               </div>
@@ -83,17 +83,17 @@
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
   <!-- Vendor JS Files -->
-  <script src="{{url('public/assets/vendor/apexcharts/apexcharts.min.js' )}}"></script>
-  <script src="{{url('public/assets/vendor/bootstrap/js/bootstrap.bundle.min.js' )}}"></script>
-  <script src="{{url('public/assets/vendor/chart.js/chart.umd.js' )}}"></script>
-  <script src="{{url('public/assets/vendor/echarts/echarts.min.js' )}}"></script>
-  <script src="{{url('public/assets/vendor/quill/quill.js' )}}"></script>
-  <script src="{{url('public/assets/vendor/simple-datatables/simple-datatables.js' )}}"></script>
-  <script src="{{url('public/assets/vendor/tinymce/tinymce.min.js' )}}"></script>
-  <script src="{{url('public/assets/vendor/php-email-form/validate.js' )}}"></script>
+  <script src="{{url('assets/vendor/apexcharts/apexcharts.min.js' )}}"></script>
+  <script src="{{url('assets/vendor/bootstrap/js/bootstrap.bundle.min.js' )}}"></script>
+  <script src="{{url('assets/vendor/chart.js/chart.umd.js' )}}"></script>
+  <script src="{{url('assets/vendor/echarts/echarts.min.js' )}}"></script>
+  <script src="{{url('assets/vendor/quill/quill.js' )}}"></script>
+  <script src="{{url('assets/vendor/simple-datatables/simple-datatables.js' )}}"></script>
+  <script src="{{url('assets/vendor/tinymce/tinymce.min.js' )}}"></script>
+  <script src="{{url('assets/vendor/php-email-form/validate.js' )}}"></script>
 
   <!-- Template Main JS File -->
-  <script src="{{url('public/assets/js/main.js' )}}"></script>
+  <script src="{{url('assets/js/main.js' )}}"></script>
 
 
    <!-- Toast JavaScript -->
